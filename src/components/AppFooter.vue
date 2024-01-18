3d7dc1cc199135587163909413f25f0cc0ac6bec
ClaudioScordari <scordari22@gmail.com>
2024-01-18 19:37:57 +0100

    Add footer - Completato esercizio base

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -19,9 +19,269 @@ export default {
             <div class="container">
                     
                 <div class="col">
-                    colonna
+                    <h3>
+                        DC COMICS
+                    </h3>
+
+                    <br>
+
+                    <ul>
+                        <li>
+                            <a href="">
+                                Characters
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Comics
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Movies
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Characters
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Comics
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Movies
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Movies
+                            </a>
+                        </li>
+                    </ul>
+
+                    <br>
+                    
+                    <h3>
+                        SHOP
+                    </h3>
+
+                    <br>
+
+                    <ul>
+                        <li>
+                            <a href="">
+                                Shop Dc
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Shop Dc Collectibles
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col">
+                    <h3>
+                        DC COMICS
+                    </h3>
+
+                    <br>
+
+                    <ul>
+                        <li>
+                            <a href="">
+                                Characters
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Comics
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Movies
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Characters
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Comics
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Movies
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Movies
+                            </a>
+                        </li>
+                    </ul>
+
+                    <br>
+                    
+                    <h3>
+                        SHOP
+                    </h3>
+
+                    <br>
+
+                    <ul>
+                        <li>
+                            <a href="">
+                                Shop Dc
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Shop Dc Collectibles
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col">
+                    <h3>
+                        DC COMICS
+                    </h3>
+
+                    <br>
+
+                    <ul>
+                        <li>
+                            <a href="">
+                                Characters
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Comics
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Movies
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Characters
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Comics
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Movies
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Movies
+                            </a>
+                        </li>
+                    </ul>
+
+                    <br>
+                    
+                    <h3>
+                        SHOP
+                    </h3>
+
+                    <br>
+
+                    <ul>
+                        <li>
+                            <a href="">
+                                Shop Dc
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                Shop Dc Collectibles
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <img src="/public/img/dc-logo-bg.png" alt="">
                 </div>
     
+            </div>
+        </section>
+
+        <section>
+            <div class="container">
+                
+                <div>
+                    <a class="sign_up-button" href="">
+                        SIGN-UP NOW!
+                    </a>
+                </div>
+
+                <div>
+                    <h3>
+                        FOLLOW US
+                    </h3>
+
+                    <a href="">
+                        <img src="/public/img/footer-facebook.png" alt="">
+                    </a>
+
+                    <a href="">
+                        <img src="/public/img/footer-periscope.png" alt="">
+                    </a>
+
+                    <a href="">
+                        <img src="/public/img/footer-pinterest.png" alt="">
+                    </a>
+
+                    <a href="">
+                        <img src="/public/img/footer-twitter.png" alt="">
+                    </a>
+
+                    <a href="">
+                        <img src="/public/img/footer-youtube.png" alt="">
+                    </a>
+                </div>
+
             </div>
         </section>
     </footer>
@@ -37,10 +297,71 @@ export default {
 
     footer{
         > section:nth-of-type(1){
-            background-image: url('/public/img/');
+            background-image: url('/public/img/footer-bg.jpg');
+            object-fit: contain;
             > .container {
-                .col{
-                    width: 20%;
+                display: flex;
+                align-items: center;
+                > .col{
+                    width: 15%;
+
+                    h3{
+                        color: white;
+                    }
+
+                    ul{
+                        list-style: none;
+
+                        li{
+                            margin-top: 5px;
+                        }
+
+                        a{
+                            text-decoration: none;
+                            color: grey;
+                        }
+                    }
+                }
+
+                > div:nth-of-type(4){
+                    flex-grow: 1;
+                    
+                    img{
+                        width: 400px;
+                        height: 400px;
+                    }
+                }
+            }
+        }
+
+        > section:nth-of-type(2){
+            background-color: #303030;
+            .container{
+                display: flex;
+                justify-content: space-between;
+                padding-top: 40px;
+                padding-bottom: 40px;
+
+                .sign_up-button{
+                    text-decoration: none;
+                    color: white;
+                    font-size: 25px;
+                    border: 4px solid lightblue;
+                    padding: 15px;
+                }
+
+                > div:nth-of-type(2){
+                    display: flex;
+                    align-items: center;
+
+                    h3{
+                        color: lightblue;
+                    }
+
+                    *:nth-child(2n) {
+                        margin-left: 18px;
+                        margin-right: 18px;
+                    }
                 }
             }
         }

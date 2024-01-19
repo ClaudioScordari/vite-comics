ee9c2ad72d3e4b6db8f5365bcf5ca11d3b1e97fc
ClaudioScordari <scordari22@gmail.com>
2024-01-19 16:22:08 +0100

    Esercizio base completato

---
+++ src/components/AppMyCard.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    data(){
+        return {
+           
+        }
+    },
+    props:{
+        comic: Object
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<!-- ------------------------------------------------------------------------------- -->
+
+<template>
+    <div class="one-comic">
+        <div class="img-comic">
+            <img :src="comic.thumb" :alt="comic.series">
+        </div>
+
+        <br>
+
+        <div>
+            {{ comic.series }}
+        </div>
+    </div>
+</template>
+
+<!-- ------------------------------------------------------------------------------- -->
+
+<style lang="scss" scoped>
+
+img{
+    width: 100%;
+    height: 280px;
+    display: block;
+}
+.one-comic{
+    width: calc((100% / 6) - 15px);
+    margin-bottom: 20px;
+}
+</style>
